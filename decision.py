c458a185a68dfdd7670418efbd98682c6c8f57cd
Jitesh Sharma Github <sjitesh85@gmail.com>
2026-06-17 13:58:18 +0300

    temp

--- decision.py
+++ decision.py
@@ -1,7 +1,15 @@
 # the Open weather API
 
-import datetime 
+
 import requests
-x = requests.get(url="https://api.openweathermap.org/data/2.5/weather?lat=54.6845&lon=25.2858&units=metric&appid=cf5781a1d377581680898bcb7d5e0d5c")
-print(x)
-print("hi") 
+
+#54.685500, 25.287539 - cathedral square
+weather = requests.get(url="https://api.openweathermap.org/data/2.5/weather?lat=54.685500&lon=25.287539&units=metric&appid=cf5781a1d377581680898bcb7d5e0d5c")
+weather.raise_for_status()
+
+data = weather.json()
+
+temp = weather.json()["main"]["temp"]
+print("So, the current temperature is: ", temp)
+
+
