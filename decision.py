eed7b4a3f40845a7ac8bd6f48d267670e07aa012
Jitesh Sharma Github <sjitesh85@gmail.com>
2026-06-17 17:31:16 +0300

    adding clothes

--- decision.py
+++ decision.py
@@ -9,7 +9,42 @@ weather.raise_for_status()
 
 data = weather.json()
 
-temp = weather.json()["main"]["temp"]
-print("So, the current temperature is: ", temp)
 
+
+
+temp = weather.json()["main"]["temp"]
+
+import random
+# Fixed pant options
+pant_options = ["Black Jeans", "Blue Jeans", "Brown Pant", "Olive pant", "Grey Jeans"]
+light_jackets = ["Denim jacket", "Black Jacket", "Brown Jacket", "Red Jacket"]
+
+# Random Shirt/T-shirt options
+if temp < 10:
+    top_options = ["Black Hoddie", "Red Hoddie", "Green Hoddie"]
+    jacket_required = True
+    jacket = "Heavy jacket"
+elif 10<= temp < 20:
+    top_options = ["Blue Shirt", "Yellow Shirt", "Black Shirt"]
+    jacket_required = True
+    jacket = random.choice(light_jackets)
+else:
+    top_options = ["Black T-Shirt", "Olive T-Shirt", "White T-Shirt", "Blue T-Shirt", "Grey T-Shirt"]
+    jacket_required = False
+    jacket = None
+
+# randomizer
+top = random.choice(top_options)
+pant = random.choice(pant_options)
+
+if jacket_required:
+    outfit = (top, jacket, pant)
+else:
+    outfit = (top, pant)
+
+
+
+# Output 
+print("So, the current temperature is: ", temp)
+print(f"Today's dress code is : {outfit}")
 
