bd215c6be4dd0847decdce31842bd6cf69ef532a
Jitesh Sharma Github <sjitesh85@gmail.com>
2026-06-17 17:34:54 +0300

    edit out extra lines

--- decision.py
+++ decision.py
@@ -7,10 +7,7 @@ import requests
 weather = requests.get(url="https://api.openweathermap.org/data/2.5/weather?lat=54.685500&lon=25.287539&units=metric&appid=cf5781a1d377581680898bcb7d5e0d5c")
 weather.raise_for_status()
 
-data = weather.json()
-
-
-
+#data = weather.json()
 
 temp = weather.json()["main"]["temp"]
 
